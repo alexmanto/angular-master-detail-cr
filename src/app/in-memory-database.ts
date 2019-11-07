eae3005f82ff13b8a3fbab20e93db30f158d258f
alexmanto <40876399+alexmanto@users.noreply.github.com>
2019-11-06 23:09:15 -0300

    adding model and db

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,17 @@
+import { InMemoryDbService } from "angular-in-memory-web-api";
+
+import { Category } from "./pages/categories/shared/category.model";
+
+export class InMemoryDatabase implements InMemoryDbService {
+    createDb(){
+        const categories = [
+            { id: 1, name: "Moradia", description: "Pagamentos de contas da casa" },
+            { id: 2, name: "Saúde", description: "Plano de saúde e remédios" },
+            { id: 3, name: "Lazer", description: "Cinema, parques, praia e etc" },
+            { id: 4, name: "Salário", description: "Recebimento de salário" },
+            { id: 5, name: "Freelas", description: "Trabalhos como freelancer" }
+        ];
+
+        return { categories }
+    }
+}
